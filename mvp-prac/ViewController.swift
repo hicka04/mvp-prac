3155803727079d1a391e3c7975405a80f12874fe
sato-hikaru <calpissoda8221@gmail.com>
2017-09-27 23:49:46 +0900

    Initial Commit

---
+++ mvp-prac/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mvp-prac
+//
+//  Created by HikaruSato on 2017/09/27.
+//  Copyright © 2017年 HikaruSato. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
